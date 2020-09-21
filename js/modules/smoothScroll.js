d438d53381bf246549233518cf8a6d8903aa2674
m0nch1 <m0nch1.develop@gmail.com>
2020-09-21 17:42:51 +0900

    delete comment

--- js/modules/smoothScroll.js
+++ js/modules/smoothScroll.js
@@ -29,7 +29,6 @@ export default function smoothScroll() {
 
   document.addEventListener("click", (e) => {
     const target = e.target;
-    // clickした要素がclass属性、js-smooth-scrollを含まない場合は処理を中断
     if (!target.classList.contains("js-smooth-scroll")) return;
     e.preventDefault();
     const targetId = target.hash;
